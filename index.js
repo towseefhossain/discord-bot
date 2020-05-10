254afd4579266db7f36841c1b0e3338750c4f125
M Towseef Hossain <towseef.22aug@gmail.com>
2020-05-10 06:32:39 -0400

    test commit

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const client = new Discord.Client();
 client.commands = new Discord.Collection();
 
 
+
+
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
